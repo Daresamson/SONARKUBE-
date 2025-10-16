5dc8b2ffc576ebdf3040e1ce1d721a64f4ffe2ec
Daresamson <165600918+Daresamson@users.noreply.github.com>
2025-10-16 08:29:42 -0700

    Add SonarQube configuration to GOLAND.go

---
+++ GOLAND.go
@@ -0,0 +1,6 @@
+sonar.projectKey=golang-backend
+sonar.sources=.
+sonar.language=go
+sonar.go.coverage.reportPaths=coverage.out
+sonar.host.url=http://<EC2_PUBLIC_IP>:9000
+sonar.login=${SONAR_TOKEN}
